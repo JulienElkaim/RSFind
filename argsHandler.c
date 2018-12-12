2dc93ed422d314df4bcf32610dee724c048518f5
JulienElkaim <julien.elkaim@telecomnancy.eu>
2018-12-12 23:30:47 +0100

    GETOPT implemented; Structure prepared to receive arguments code

---
+++ argsHandler.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "argsHandler.h"
+#include <string.h>
+
+int errInArg(int argc, char** argv){
+	//Si la liste des arguments est mal entrée, 
+
+	int iOption = 0;
+	int rsFindOptions;
+	int status;
+
+	if(fork()==0){//fils
+		while((rsFindOptions = getopt_long(argc, argv, "lt:in:e:p", myLongOptions, &iOption)) != -1){
+			if (optarg!=NULL){
+				if(optarg[0]=='-'){
+					exit(1);
+				} // Une erreur se produit
+			}else{
+				if (rsFindOptions=='?')
+					exit(2); //Missing an argument here
+			}
+		}exit(0);
+	}else{//pere
+		wait(&status);
+		if(WIFEXITED(status)){
+			if(WEXITSTATUS(status)==0){
+				//tout etait normal
+				return 0;
+			}else{
+				return 1;
+			}
+		}
+	}
+	
+}
+
+int optDispatcher(int argc, char** argv, int* lOpt, int* tOpt, char** tArg, int* iOpt, int* nOpt, char** nArg, int* eOpt, char** eArg, int* pOpt){
+
+	int iOption =0; //Useful to find the long_option characteristic
+	int rsFindOptions;
+	while((rsFindOptions = getopt_long(argc, argv, "lt:in:e:p", myLongOptions, &iOption)) != -1){
+		
+		switch(rsFindOptions){ 
+
+			case 'l':
+				*lOpt = 1; 
+				break;
+			case 't':
+				*tOpt = 1;
+				*tArg = malloc(sizeof(char)*(strlen(optarg)+1) );
+				memset(*tArg, 0,sizeof(char)*(strlen(optarg)+1) );
+				strcpy(*tArg, optarg);
+				break;
+			case 'i':
+				*iOpt = 1;
+				break;
+			case 'n':
+				*nOpt = 1;
+				*nArg = malloc(sizeof(char)*(strlen(optarg)+1) );
+				memset(*nArg, 0,sizeof(char)*(strlen(optarg)+1) );
+				strcpy(*nArg, optarg);
+				break;
+			
+			case 'e':
+				printf("Je passe \n");
+				*eOpt = 1;
+				*eArg = malloc(sizeof(char)*(strlen(optarg)+1) );
+				memset(*eArg, 0,sizeof(char)*(strlen(optarg)+1) );
+				strcpy(*eArg, optarg);
+				break;
+			case 'p':
+				*pOpt = 1;
+			default:
+				//invalid argument ! 
+				return 1;
+		}
+		
+	}
+	return 0;
+
+}
+
+
--- rsfind.c
+++ rsfind.c
@@ -1,5 +1,62 @@
 #include <stdio.h>
-int main () {
-printf("Hello world \n");
-return 0;
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "argsHandler.h"
+#include <string.h>
+
+
+int main(int argc, char** argv){
+	
+	int returnOferrInArg = errInArg(argc, argv);
+	if (returnOferrInArg!=0){
+		if (returnOferrInArg==1){
+			return returnOferrInArg;
+		}else if (returnOferrInArg ==2){
+			return returnOferrInArg;
+		}
+	}
+
+	int lOpt =0;
+	int tOpt =0;
+	char* tArg;
+	int iOpt =0;
+	int nOpt =0;
+	char* nArg;
+	int eOpt =0;
+	char* eArg;
+	int pOpt =0;
+
+	if(optDispatcher(argc, argv, &lOpt, &tOpt, &tArg, &iOpt, &nOpt, &nArg, &eOpt, &eArg, &pOpt)){
+		//Un mauvais argument, s'ârrête subitement
+		return 1;
+	}
+
+	/*
+	printf("lOpt is %d \n",lOpt);
+	printf("tOpt is %d and argument is %s\n",tOpt, tArg);
+	printf("iOpt is %d \n",iOpt);
+	printf("nOpt is %d and argument is %s\n",nOpt, nArg);
+	printf("eOpt is %d and argument is %s\n",eOpt, eArg);
+	printf("pOpt is %d \n",pOpt);
+	*/
+
+	//EXECUTER la recherche recursive avec dirent
+
+	//APPLIQUER les restrictions -n puis -i puis -t
+
+	//APPLIQUER le format -l
+
+	//EXEC sur le resultat avec du pipe
+
+	//PRINT
+
+
+
+
+
+
+	return 0;
 }
+
+
